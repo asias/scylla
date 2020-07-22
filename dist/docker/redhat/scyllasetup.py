926e722fd43c997b7b5f13e33e65d0292660e8a3
Avi Kivity <avi@scylladb.com>
2020-07-22 14:17:53 +0300

    Merge 'docker: Make I/O configuration setup configurable' from Pekka
    "
    This adds a '--io-setup N' command line option, which users can pass to
    specify whether they want to run the "scylla_io_setup" script or not.
    This is useful if users want to specify I/O settings themselves in
    environments such as Kubernetes, where running "iotune" is problematic.
    
    While at it, add the same option to "scylla_setup" to keep the interface
    between that script and Docker consistent.
    
    Fixes #6587
    "
    
    * penberg-penberg/docker-no-io-setup:
      scylla_setup: Add '--io-setup ENABLE' command line option
      dist/docker: Add '--io-setup ENABLE' command line option
    
    (cherry picked from commit 5e1fa13d088c5fdea7c4626474e367f81d5d1f4d)

--- dist/docker/redhat/scyllasetup.py
+++ dist/docker/redhat/scyllasetup.py
@@ -26,6 +26,7 @@ class ScyllaSetup:
         self._clusterName = arguments.clusterName
         self._endpointSnitch = arguments.endpointSnitch
         self._replaceAddressFirstBoot = arguments.replaceAddressFirstBoot
+        self._io_setup = arguments.io_setup
 
     def _run(self, *args, **kwargs):
         logging.info('running: {}'.format(args))
@@ -58,7 +59,8 @@ class ScyllaSetup:
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
 
-        self._run(['/opt/scylladb/scripts/scylla_io_setup'])
+        if self._io_setup == "1":
+            self._run(['/opt/scylladb/scripts/scylla_io_setup'])
 
     def cqlshrc(self):
         home = os.environ['HOME']
