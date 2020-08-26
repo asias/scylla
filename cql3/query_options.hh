db6c686b76628afb36665a101267aa214eb77df4
Avi Kivity <avi@scylladb.com>
2020-08-26 13:19:17 +0300

    Merge 'Move temporaries to value view' from Piotr S
    "
    Issue https://github.com/scylladb/scylla/issues/7019 describes a problem of an ever-growing map of temporary values stored in query_options. In order to mitigate this kind of problems, the storage for temporary values is moved from an external data structure to the value views itself. This way, the temporary lives only as long as it's accessible and is automatically destroyed once a request finishes. The downside is that each temporary is now allocated separately, while previously they were bundled in a single byte stream.
    
    Tests: unit(dev)
    Fixes https://github.com/scylladb/scylla/issues/7019
    "
    
    70552976491 ("cql3: remove query_options::linearize and _temporaries")
    is reverted from this backport since linearize() is still used in
    this branch.
    
    * psarna-move_temporaries_to_value_view:
      cql3: remove query_options::linearize and _temporaries
      cql3: remove make_temporary helper function
      cql3: store temporaries in-place instead of in query_options
      cql3: add temporary_value to value view
      cql3: allow moving data out of raw_value
      cql3: split values.hh into a .cc file
    
    (cherry picked from commit 2b308a973fa5642068e264abe92d72aa714c1919)

--- cql3/query_options.hh
+++ cql3/query_options.hh
@@ -167,7 +167,6 @@ public:
         return _value_views.size();
     }
 
-    cql3::raw_value_view make_temporary(cql3::raw_value value) const;
     bytes_view linearize(fragmented_temporary_buffer::view) const;
 
     bool skip_metadata() const {
