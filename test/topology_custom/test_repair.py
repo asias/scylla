ddbeb9a0567ea6e19a2fd25ff5906951045fa1bd
Asias He <asias@scylladb.com>
2024-12-31 10:21:00 +0800

    ci

--- test/topology_custom/test_repair.py
+++ test/topology_custom/test_repair.py
@@ -255,7 +255,7 @@ async def do_repair_high_rf_with_gen_data(manager, enable_opt):
     table = 'test'
     key_nr = 10000000
     key_nr = 5000000
-    key_nr = 1000000
+    key_nr = 1000000 # 16 Get rows call
     start_key = 1
     end_key = key_nr
     column_size = 34
